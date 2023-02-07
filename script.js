21f603907f0e840f26f7b0d6c0c727f36905636d
jamDEV3 <tyler_j83@outlook.com>
2023-02-07 17:44:21 +0000

    Button toggle for 12/24 mode

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-let span = document.getElementById('span');
-
-// Executes function every second
-setInterval(time, 1000);
+let clockDisplay = document.getElementById('display');
 
 function time() {
+
+    // Executes function every second
+    setInterval(time, 1);
 
     // Declares time properties
     let date = new Date();
     let hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
+    let am_pm;
+    let currentTime;
 
+    if ((clockDisplay.className == 'twelve') || (clockDisplay.className == 'time')) {
+    
     // Makes sure 2 digits appear for each time partition
     hour = hour < 10 ? "0" + hour: hour;
     min = min < 10 ? "0" + min: min;
     sec = sec < 10 ? "0" + sec: sec;
 
-    // Arranges time into appropriate digital clock format
-    let currentTime = hour + ":" + min + ":" + sec;
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
 
-    // Translates JS to HTML element
-    span.innerHTML = currentTime;
+    currentTime = hour + ":" + min + ":" + sec + am_pm;
+    clockDisplay.innerHTML = currentTime;
+    }
+
+    else if (clockDisplay.className == 'twentyFour'){
+
+    // Makes sure 2 digits appear for each time partition
+    hour = hour < 10 ? "0" + hour: hour;
+    min = min < 10 ? "0" + min: min;
+    sec = sec < 10 ? "0" + sec: sec;
+    
+    currentTime = hour + ":" + min + ":" + sec;
+    clockDisplay.innerHTML = currentTime;
+    }
+}
+
+
+function twelve() {
+    clockDisplay.className = "twelve";
+}
+
+function twentyFour() {
+    clockDisplay.className = "twentyFour";
 }
 
 time();
