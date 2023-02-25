c2c7488c28aee636a173a57e4ecbb89fdba2792d
jamDEV3 <tyler_j83@outlook.com>
2023-02-25 13:19:11 +0000

    Added circle as clock base for analog

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let clockDisplay = document.getElementById('display');
+let clockMode = document.getElementById('mode');
 
 function time() {
 
@@ -10,15 +11,21 @@ function time() {
     let hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-    let am_pm;
+    let am_pm = "AM";
     let currentTime;
 
-    if ((clockDisplay.className == 'twelve') || (clockDisplay.className == 'time')) {
+    //Default time display on startup
+    if (clockDisplay.className == 'twelve') {
     
     // Makes sure 2 digits appear for each time partition
     hour = hour < 10 ? "0" + hour: hour;
     min = min < 10 ? "0" + min: min;
     sec = sec < 10 ? "0" + sec: sec;
+
+    //Quick fix: Condition for 12 Noon so it displays PM - not 100% sure if it will interfere with 12 Midnight
+    if (hour == 12) {
+        am_pm= "PM";
+    }
 
     if (hour > 12) {
         hour -= 12;
@@ -45,7 +52,7 @@ function time() {
     }
 
     else if (clockDisplay.className == 'analog') {
-        
+        analog();
     }
 }
 
@@ -59,7 +66,27 @@ function twentyFour() {
 }
 
 function analog() {
-    clockDisplay.className = "analog";
+    clockMode.className = "analog";
+
+    analogClock = document.createElement("canvas");
+    analogClock.id = "analogClock";
+    analogClock.width = 200;
+    analogClock.height = 200;
+    document.body.appendChild(analogClock);
+
+    canvas = document.getElementById("analogClock");
+
+    ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.arc(100, 100, 100, 0, 2 * Math.PI);
+    ctx.stroke();
+
+}
+
+function digital() {
+    clockMode.className = "digital";
+    canvas.remove();
 }
 
 time();
